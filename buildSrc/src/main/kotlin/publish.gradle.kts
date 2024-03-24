b047d9c8b8f77b97a03ae5884a9c98d8e0351112
Arnaud 'Bluexin' Solé <bluexin.games@gmail.com>
2024-03-24 22:45:23 +0100

    Fix publishing URL

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -20,7 +20,7 @@ publishing {
             url = uri(rootProject.layout.buildDirectory.get().asFile.toURI().resolve("localPublication"))
         } else if (System.getenv("CI") != null) maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/octocat/hello-world")
+            url = uri("https://maven.pkg.github.com/bluexin/luaj-ksp")
             credentials {
                 username = System.getenv("USERNAME")
                 password = System.getenv("TOKEN")
