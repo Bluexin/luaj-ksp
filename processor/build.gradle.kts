60f8aaf1d1c452fb197832c0010ab884d016f285
Arnaud 'Bluexin' Solé <bluexin.games@gmail.com>
2024-04-03 00:33:14 +0200

    Test receiver KFunction var processing

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     testImplementation("dev.zacsweers.kctfork:ksp:0.4.1")
     testImplementation("com.github.wagyourtail.luaj:luaj-jse:f062b53a34")
     testImplementation(kotlin("reflect"))
+    testImplementation("io.mockk:mockk:1.13.10")
 }
 
 tasks.test {
--- processor/src/test/kotlin/be/bluexin/luajksp/BuiltInTypesTest.kt
+++ processor/src/test/kotlin/be/bluexin/luajksp/BuiltInTypesTest.kt
@@ -2,23 +2,28 @@ package be.bluexin.luajksp
 
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.luaj.vm2.LuaError
+import org.luaj.vm2.LuaFunction
 import org.luaj.vm2.LuaUserdata
 import org.luaj.vm2.LuaValue
+import org.luaj.vm2.Varargs
 import org.luaj.vm2.lib.OneArgFunction
-import kotlin.test.Test
-import kotlin.test.assertContains
-import kotlin.test.assertEquals
-import kotlin.test.assertIs
+import org.luaj.vm2.lib.TwoArgFunction
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.test.*
 
 @OptIn(ExperimentalCompilerApi::class)
 class BuiltInTypesTest: LKSymbolProcessorTest() {
 
     @Test
-    fun `test unit return type processing`() {
+    fun `test unit return type KFunction processing`() {
         val kotlinSource = SourceFile.kotlin(
             "KClass.kt", """
                     import be.bluexin.luajksp.annotations.LuajExpose
@@ -52,5 +57,76 @@ class BuiltInTypesTest: LKSymbolProcessorTest() {
         assertIs<OneArgFunction>(cb)
 
         assertThrows<E> { cb.call(LuaValue.valueOf(42.0)) }
+    }
+
+    @Test
+    fun `test receiver KFunction processing`() {
+        val kotlinSource = SourceFile.kotlin(
+            "KClass.kt", """
+                    import be.bluexin.luajksp.annotations.LuajExpose
+
+                    @LuajExpose
+                    data class KClass(var cb: KClass.(Int) -> Boolean)
+                """
+        )
+
+        val result = compile(kotlinSource)
+
+        assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
+
+        // Diagnostics
+        assertContains(result.messages, "Generating access.KClassAccess for KClass")
+
+        assertDoesNotThrow {
+            result.classLoader.loadClass("access.KClassAccess")
+        }
+
+        val data = result.instance("KClass", { _: Any /* yes, hax */, _: Int -> false })
+        val access = result.instance("access.KClassAccess",  data)
+
+        assertIs<LuaUserdata>(access)
+        val cb = assertDoesNotThrow {
+            access.get("cb")
+        }
+
+        assertIs<TwoArgFunction>(cb)
+
+        val args = slot<Varargs>()
+        val callback = mockk<LuaFunction> {
+            every { checkfunction() } returns this
+            every { this@mockk.invoke(capture(args)) } returns LuaValue.TRUE
+        }
+
+        assertDoesNotThrow {
+            access.set("cb", callback)
+        }
+
+        val cbProp = data::class.declaredMemberProperties.singleOrNull()
+        assertNotNull(cbProp)
+        assertIs<KProperty1<Any, *>>(cbProp) // Just to make compiler happy, this has no value
+        val wrappedCb = cbProp(data)
+        assertIs<Any.(Int) -> Boolean>(wrappedCb) // will only check for KFunction2
+        val res = wrappedCb(data, 42)
+        assertIs<Boolean>(res)
+        assertTrue(res)
+
+        assertTrue(args.isCaptured)
+        val captured = args.captured
+        assertEquals(2, captured.narg())
+        assertEquals(access, captured.arg1())
+        assertEquals(42, captured.arg(2).checkint())
+
+        val data2 = result.instance("KClass", { _: Any /* yes, hax */, _: Int -> false })
+
+        args.clear()
+        val res2 = wrappedCb(data2, 71)
+        assertIs<Boolean>(res2)
+        assertTrue(res2)
+
+        assertTrue(args.isCaptured)
+        val captured2 = args.captured
+        assertEquals(2, captured2.narg())
+        assertNotEquals(access, captured2.arg1())
+        assertEquals(71, captured2.arg(2).checkint())
     }
 }
